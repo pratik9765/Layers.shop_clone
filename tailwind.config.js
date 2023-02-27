2bacebcaa793b9c997ebaa121980a7a5dc3bb05b
pratik9765 <pratikbokde9765@gmail.com>
2023-02-27 09:34:28 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFfamily: {
+        Prompt: ['Prompt', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
